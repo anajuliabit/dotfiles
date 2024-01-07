ca04da691c5916eb46ea5e9bea4661f35b6b2a47
Ana Julia <anajuliabit@gmail.com>
2024-01-07 16:54:45 -0300

    evil + straight + default conf refactor

---
+++ modules/emacs/features/feat.editor.el
@@ -0,0 +1,210 @@
+;;; feat.editor.el --- Editor improvements -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;;; Replacements and improvements for Emacs behaviour
+
+;;; Code:
+;;; Clean emacs configuration with no littering
+(use-package no-littering
+  :straight t
+  :init
+  (require 'recentf)
+  :config
+  (setq auto-save-file-name-transforms
+	`((".*" ,(no-littering-expand-var-file-name "auto-save/") t)))
+  (add-to-list 'recentf-exclude no-littering-var-directory)
+  (add-to-list 'recentf-exclude no-littering-etc-directory))
+
+;;; Diminish
+(use-package diminish
+  :straight t)
+
+;;; counsel
+(use-package counsel
+  :straight t
+  :bind
+  (("M-x" . counsel-M-x)
+   ("C-c g" . counsel-ag)
+   ("C-x C-f" . counsel-find-file)))
+
+(use-package counsel-projectile
+  :straight t
+  :after projectile)
+
+;;; ivy - generic completion frontend
+(use-package ivy
+  :straight t
+  :diminish ivy-mode
+  :config
+  (setq enable-recursive-minibuffers nil)
+  (setq ivy-display-style 'fancy)
+  (setq ivy-height 35)
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-magic-slash-non-match-action nil)
+  (defun swiper-recenter ()
+    "Advice swiper to recenter on exit."
+    (recenter))
+  (advice-add 'swiper :after #'swiper-recenter)
+  (ivy-mode t))
+
+(use-package wgrep-ag
+  :straight t)
+
+(use-package ivy-rich
+  :straight t
+  :config
+  (ivy-rich-mode t))
+
+(use-package lsp-ivy
+  :straight t)
+
+;;; swiper - isearch replacement
+(use-package swiper
+  :straight t
+  :bind
+  (("C-s" . swiper)))
+
+;;; avy - jump to characters and expressions fast
+(use-package avy
+  :straight t
+  :custom-face
+  :config
+  (setq avy-background t)
+  (setq avy-all-windows t)
+  (custom-set-faces
+   '(avy-lead-face
+     ((t (:inherit avy-lead-face
+                   :background "#eeeeee"
+                   :foreground "#424242"))))
+   '(avy-lead-face-0
+     ((t (:inherit avy-lead-face-0
+                   :background "#feca32"
+                   :foreground "#424242")))))
+  :bind
+  (("C-c a" . avy-goto-char)
+   ("C-c o" . avy-goto-char-timer)
+   ("C-c e" . swiper-avy)))
+
+;;; Project management
+(use-package projectile
+  :straight t
+  :diminish projectile-mode
+  :config
+  (projectile-mode t)
+:bind (:map projectile-mode-map
+              ("s-p" . projectile-command-map)
+              ("C-c p" . projectile-command-map)))
+
+;;; Smart M-x enhancement, e.g. sorting items in M-x minibuffer by usage
+(use-package smex
+  :straight t)
+
+;;; Switch window
+(use-package switch-window
+  :straight t)
+
+;;; Execute path from shell
+(use-package exec-path-from-shell
+  :straight t
+  :config
+  (exec-path-from-shell-initialize))
+
+;;; Undo visualization
+(use-package undo-tree
+  :disabled
+  :straight t
+  :diminish undo-tree-mode)
+
+;;; Smart hungry delete
+(use-package smart-hungry-delete
+  :straight t
+  :bind
+  (("C-<backspace>" . smart-hungry-delete-backward-char)
+   ("C-d" . smart-hungry-delete-forward-char))
+  :config
+  (smart-hungry-delete-add-default-hooks))
+
+;; Highlight parantheses
+(use-package highlight-parentheses
+  :straight t
+  :hook ((prog-mode . highlight-parentheses-mode)
+         (prog-mode . show-paren-mode)))
+
+;; Treemacs
+(use-package treemacs
+  :straight t
+  :config
+  (setq treemacs-position 'left)
+  (setq treemacs-width 42)
+  (setq treemacs-indentation 2)
+  (setq treemacs-space-between-root-nodes nil)
+  (treemacs-resize-icons 13)
+  (treemacs-fringe-indicator-mode 'always)
+  (treemacs-filewatch-mode t)
+  (treemacs-git-mode 'deferred)
+  :hook
+  (treemacs-mode . (lambda ()
+                     (display-line-numbers-mode -1)
+                     (custom-set-faces
+                      '(treemacs-root-face
+                        ((t (:inherit treemacs-root-face
+                             :family "Iosevka Term SS08 Thin"
+                             :height 115))))))))
+
+(use-package treemacs-evil
+  :after treemacs evil
+  :straight t)
+
+(use-package treemacs-projectile
+  :after treemacs projectile
+  :straight t)
+
+(use-package treemacs-icons-dired
+  :after treemacs dired
+  :straight t
+  :config (treemacs-icons-dired-mode))
+
+(use-package treemacs-magit
+  :after treemacs magit
+  :straight t)
+
+;; dashboard
+(use-package dashboard
+  :straight t
+  :init
+  (dashboard-setup-startup-hook)
+  (setq dashboard-center-content t)
+  (setq dashboard-items '((recents  . 10)
+                          (projects . 3)
+                          (registers . 5)))
+  (setq dashboard-set-heading-icons t)
+  (setq dashboard-set-file-icons t)
+  (setq dashboard-set-navigator t)
+  (setq dashboard-set-footer nil)
+  (setq dashboard-banner-logo-title "Have a good coding session")
+  (setq initial-buffer-choice (lambda () (get-buffer-create "*dashboard*"))))
+
+;; view large files
+(use-package vlf
+  :straight t
+  :config
+  (require 'vlf-setup))
+
+;; string inflection
+(use-package string-inflection
+  :straight t
+  :config
+  (require 'string-inflection))
+
+;; log files
+(use-package logview
+  :straight t)
+
+;; editorconfig
+(use-package editorconfig
+  :straight t
+  :config
+  (editorconfig-mode 1))
+
+(provide 'feat.editor)
+;;; feat.editor.el ends here
--- modules/emacs/init.el
+++ modules/emacs/init.el
@@ -1,117 +1,23 @@
-(setq inhibit-startup-message t)
-(defvar chidori-cache-dir (expand-file-name "cache/" user-emacs-directory))
+;;; init.el --- Main entry for Emacs configuration -*- lexical-binding: t; -*-
 
-(scroll-bar-mode -1)        ; Disable visible scrollbar
-(tool-bar-mode -1)          ; Disable the toolbar
-(tooltip-mode -1)           ; Disable tooltips
-(set-fringe-mode 10)        ; Give some breathing room
-
-(menu-bar-mode -1)            ; Disable the menu bar
-
-
-;; Fullscreen by default
-(add-to-list 'default-frame-alist '(fullscreen . maximized))
-
-;; Disable backup files
-(setq make-backup-files nil)
-
-(set-face-attribute 'default nil :font "Fira Mono" :height 140)
-
-(column-number-mode)
-(global-display-line-numbers-mode t)
+;;; Commentary:
+;;; Code:
+;; set nix-dotfiles path
+(defvar config-path (expand-file-name "~/nix-dotfiles/modules/emacs")
+  "Path to nix-dotfiles.")
+(load (expand-file-name "core/core.configuration.el" config-path))
+(load (expand-file-name "core/core.straight.el" config-path))
+(load (expand-file-name "features/feat.completion.el" config-path))
+(load (expand-file-name "features/feat.editor.el" config-path))
+(load (expand-file-name "features/feat.evil.el" config-path))
+(load (expand-file-name "languages/lang.typescript.el" config-path))
 
 (dolist (mode '(org-mode-hook
                 term-mode-hook
 	        eshell-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
-;; Make ESC quit prompts
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
-
-;; Initialize package sources
-(require 'package)
-
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")
-                         ("elpa" . "https://elpa.gnu.org/packages/")))
-
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
-
-;; Initialize use-package on non-Linux platforms
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
-
-(require 'use-package)
 (setq use-package-always-ensure t)
-
-;; straight
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name
-        "straight/repos/straight.el/bootstrap.el"
-        (or (bound-and-true-p straight-base-dir)
-            user-emacs-directory)))
-      (bootstrap-version 7))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
-
-(use-package ivy
-  :diminish
-  :bind (("C-s" . swiper)
-         :map ivy-minibuffer-map
-         ("TAB" . ivy-alt-done)
-         ("C-l" . ivy-alt-done)
-         ("C-j" . ivy-next-line)
-         ("C-k" . ivy-previous-line)
-         :map ivy-switch-buffer-map
-         ("C-k" . ivy-previous-line)
-         ("C-l" . ivy-done)
-         ("C-d" . ivy-switch-buffer-kill)
-         :map ivy-reverse-i-search-map
-
-	 ("C-k" . ivy-previous-line)
-         ("C-d" . ivy-reverse-i-search-kill))
-  :config
-  (ivy-mode 1)
-  (setq ivy-use-selectable-prompt t)
-  )
-
-
-(use-package counsel
-  :ensure t
-  :init (counsel-mode 1)
-  :bind (("M-x" . counsel-M-x)
-	 ("C-x b" . counsel-ibuffer)
-	 ("C-x C-f" . counsel-find-file)
-	 :map minibuffer-local-map
-	 ("C-r". counsel-minibuffer-history))
-  )
-
-(setq evil-want-keybinding nil)
-
-(use-package evil
-  :ensure t
-  :config
-  (evil-mode 1)
-  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
-  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
-  (evil-set-initial-state 'messages-buffer-mode 'normal)
-  (evil-set-initial-state 'dashboard-mode 'normal)
-  )
-
-(use-package evil-collection
-  :after evil
-  :ensure t
-  :config
-  (evil-collection-init))
 
 (use-package all-the-icons
   :ensure t
@@ -202,9 +108,6 @@
                          (executable-find "nomicfoundation-solidity-language-server"))
                 (lsp-deferred)))))
 
-(add-hook 'js-mode-hook #'lsp)
-(add-hook 'typescript-mode-hook #'lsp)
-
 (use-package treesit-auto
   :custom
   (treesit-auto-install 'prompt)
@@ -212,52 +115,7 @@
   (treesit-auto-add-to-auto-mode-alist 'all)
   (global-treesit-auto-mode))
 
-(use-package lsp-mode
-  :defer t
-  :straight t
-  :hook ((nix-mode . lsp)
-         (rustic-mode . lsp)
-         (solidity-mode . lsp)
-         (lisp-mode . lsp-deferred)
-         (js-mode . lsp-deferred)
-         (js2-mode . lsp-deferred)
-         (typescript-mode . lsp-deferred))
-  :commands (lsp lsp-deferred lsp-register-client)
-  :config
-  (lsp-enable-which-key-integration t)
-  (setq lsp-lens-enable t)
-  (lsp-register-client
-   (make-lsp-client
-    :new-connection (lsp-stdio-connection '("nomicfoundation-solidity-language-server" "--stdio"))
-    :major-modes '(solidity-mode)
-    :priority -1
-    :server-id 'solidity-ls))
-  )
-
-(use-package lsp-ui
-  :after lsp-mode
-  :straight t
-  :hook (lsp-mode . lsp-ui-mode)
-  :config
-  (setq lsp-ui-doc-position 'bottom
-        lsp-ui-sideline-delay 0.5
-        lsp-ui-sideline-enable nil))
-
 (provide 'pkg-solidity-mode)
-;;; package --- Company (autocomplete) settings
-(use-package company
-  :ensure t
-  :after (lsp-mode)
-  :commands (company-mode)
-  :bind (:map company-active-map
-         ("<tab>" . company-complete-selection)
-         :map lsp-mode-map
-         ("<tab>" . company-indent-or-complete-common))
-  :config
-  (global-company-mode 1)
-  :custom
-  (company-minimum-prefix-length 3)
-  (company-idle-delay 0.0))
 
 (use-package flycheck
   :ensure t
@@ -280,15 +138,6 @@
 (use-package forge
   :straight t
   :after magit)
-
-(use-package projectile
-  :ensure t
-  :init
-
-  (projectile-mode +1)
-  :bind (:map projectile-mode-map
-              ("s-p" . projectile-command-map)
-              ("C-c p" . projectile-command-map)))
 
 (use-package copilot
   :straight (:host github :repo "zerolfx/copilot.el" :files ("dist" "*.el"))
@@ -329,6 +178,5 @@
 
 (use-package nix-mode :mode "\\.nix\\'")
 
-(add-to-list 'load-path "~/nix-dotfiles/modules/emacs/languages")
-(require 'lang.typescript)
 (provide 'init.el)
+;;; init.el ends here
